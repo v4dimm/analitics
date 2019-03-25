e9ceb02b88b6ed9bf0c3c53d8c50ce8fb1ea5c1a
v4dimm <v4dim.mv@yandex.ru>
2019-03-25 04:48:50 +0000

    Working of ranging

--- js/first.js
+++ js/first.js
@@ -9,12 +9,14 @@ function test() {
 			} else {
 				
 				div2 = createTable(experts, alternativs);
+				div2.id = 'table'
 
 				var button = document.createElement('button')
 				button.textContent = 'Принять';
 				div2.appendChild(button);
 			
 				button.addEventListener('click', function() {
+					var finishArray = [];
 
 					for (var i = 1; i < experts + 1; i++) {
 						var tr = document.getElementById('input-form-' + i);
@@ -24,25 +26,20 @@ function test() {
 						for (var j = 1; j < alternativs + 1; j++) {
 							array.push(Number(tr.childNodes[j].childNodes[0].value));
 						}
+						finishArray.push(sortRanges(array));
+					}
 
-						for (var z = 1; z < array.length + 1; z++) {
-							var MAX = array.indexOf(Math.max.apply(null, array));
-							newArray[MAX] = z;
-							array[MAX] = 0;
-						}
-
-						
-
-						console.log(newArray + '\n========');
+				if (document.getElementById('newTable')) {
+				} else {
+					div3 = createTable(experts, alternativs, finishArray);
+					div3.id = 'newTable';
 					}
 				});
 			}
 }
 
-
-function createTable(experts, alternativs) {
-	var div2 = document.createElement('div');
-	// div2.id = 'table'
+function createTable(experts, alternativs, values) {
+	var divBlock = document.createElement('div');
 	var table = document.createElement('table');
 	
 
@@ -65,19 +62,56 @@ function createTable(experts, alternativs) {
 				cell.width = 75;
 			}
 			else {
-				var input = document.createElement('input');
+				if (values) {
+					cell.innerHTML = values[i-1][j-1];
+				}
+				else {
+					var input = document.createElement('input');
 
-				input.type = 'number';
-				cell.appendChild(input);
-				input.min = 1;
-				input.max = 100;
-				input.step = 1;
-				// input.className = 'input-form';
+					input.type = 'number';
+					cell.appendChild(input);
+					input.min = 1;
+					input.max = 100;
+					input.step = 1;
+					// input.className = 'input-form';
+				}
 			}
 		}
 	}
 	
-	div2.appendChild(table);
-	document.body.getElementsByClassName('menu state')[0].appendChild(div2);
-	return div2
+	divBlock.appendChild(table);
+	document.body.getElementsByClassName('menu state')[0].appendChild(divBlock);
+	return divBlock
+}
+
+function sortRanges(array) {
+	var completeArray = [];
+	var range = 1;
+
+	while (range < array.length + 1) {
+		var sortArray = [];
+		var someChar = Number(0);
+
+		for (var i = 0; i <  array.length; i++) {
+			sortArray.push(array.indexOf(Math.max.apply(null, array), i));
+		}
+		sortArray = sortArray.filter(function(item, pos)  {
+			if (item > -1) {
+				return sortArray.indexOf(item) == pos;
+			}
+		})
+
+		for (var k = 0; k<sortArray.length; k++) {
+			someChar = someChar + range;
+			range++;
+		}
+
+		someChar = someChar / Number(sortArray.length);
+
+		for (var j=0; j < sortArray.length; j++) {
+			completeArray[sortArray[j]] = someChar;
+			array[sortArray[j]] = 0;
+		}
+	}
+	return completeArray;
 }
